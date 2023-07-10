9eee6c1d64d950e919057dfce68defa222b07cae
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-10 14:14:16 +0700

    TEST

--- test_bot/__init__.py
+++ test_bot/__init__.py
@@ -2,8 +2,5 @@
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 from . import (
-    dev_model_type,
-    dev_model,
-    dev_model_detail,
-    account_move,
+    models,
 )
